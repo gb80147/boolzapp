f4df93928e9916cc5235b5ba36f27886eb53876f
gb80147 <giuliobelfiore@gmail.com>
2019-03-14 22:54:26 +0100

    manca cancella msg

--- script.js
+++ script.js
@@ -69,12 +69,12 @@ function transferToChat(e) {
 
 function searchContact() {
 
-  var content = $(this).val();
+  var content = $(this).val().toLowerCase();
   var listName = $(".name b");
 
   for (var i = 0; i < listName.length; i++) {
 
-    var name = listName.eq(i).text();
+    var name = listName.eq(i).text().toLowerCase();
     var boxContact = $(".contact").eq(i);
 
     if (!name.includes(content)) {
@@ -87,13 +87,48 @@ function searchContact() {
   }
 }
 
+function contactClick() {
+
+  var clickedContact = $(this).index();                                //assegno l'indice di contact come "clickedContact"
+
+  var chat = $(".chat");
+  chat.removeClass("selected");                                       //tolgo visibilità della chat
+
+  var selectedChat = chat.eq(clickedContact);
+  selectedChat.addClass("selected");                                  //aggiunge classe "selected" all'index chat corrispondente al contact
+
+  var headerChat = $(".headerChat");
+  headerChat.removeClass("hidden");                                   //rendo visibile l'header della chat
+
+  var nameContactChat = $(".nameContactChat");
+  nameContactChat.addClass("hidden");                                 //nascondo tutti i "nameContactChat" aggiungendo la classe "hidden"
+
+  var selectedNameContactChat = nameContactChat.eq(clickedContact);   //dico che l'index di "nameContactChat" corrisponde a quello dei contact
+  selectedNameContactChat.removeClass("hidden")                       //all'index cliccato verrà rimossa la classe "hidden"
+
+  var mainPage = $(".main");
+  mainPage.addClass("hidden");                                        //appena clicco su un contact, la pagina principale viene nascosta
+
+  var inputMessageContainer = $(".inputMessageContainer");
+  inputMessageContainer.removeClass("hidden");                       //appena clicco su un contact, rendo visibile l'input di testo in basso
+
+  var contact = $(".contact");
+  contact.removeClass("active")                                      //rimuovo a tutti i "contact" la classe "active"
+
+  $(this).addClass("active");                                        //all'elemento cliccato viene aggiunta la classe "active"
+
+}
+
 function init(){
 
   var send = $("#sendMessage");
-  send.keyup(transferToChat);
+  send.keyup(transferToChat);                                         //evento "keyup" che si verifica al rilascio di un tasto della tastiera
 
   var nameSearch = $(".search input");
   nameSearch.keyup(searchContact);
+
+  var contact = $(".contact");
+  contact.click(contactClick);
 }
 
-$(document).ready(init);                              //quando il documento è stato caricato parte la funzione init
+$(document).ready(init);                                              //quando il documento è stato caricato parte la funzione init
